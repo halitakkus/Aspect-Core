c177d1073252017fe53598f818ca4f1cbba5ee60
halitakkus <halitakkus03@gmail.com>
2021-01-01 19:37:06 +0300

    Yılın ilk commiti 🤪

---
+++ Core/Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public async static void Validate(IValidator validator,object entity)
+        {
+           var result = await validator.ValidateAsync(entity);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
