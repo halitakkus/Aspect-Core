13ddbfec9183882ffef55caec72f2244f08bc3eb
halitakkus <halitakkus03@gmail.com>
2021-01-01 20:31:51 +0300

    Method interceptor yazıldı. Methotların OnBefore, OnAfter,OnSuccess,OnException durumları tasarlandı.

---
+++ Core/Core/CrossCuttingConcerns/Unilities/Messages/AspectMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Unilities.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Wrong Validation Type";
+    }
+}
